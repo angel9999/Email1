9e41df5c30cd0648fff40a747ee156e57c16e9b3
Alexander Alyakrinsky <32340760+AlexanderGHA@users.noreply.github.com>
2019-02-13 11:58:57 -0800

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -40,6 +40,7 @@ def home():
 
 @app.route('/posted', methods=['POST'])
 def post():
+    return render_template('hme.html')
     #This function should add the new post to the JSON file of posts and then render home.html and display the posts.  
     #Every post should include the username of the poster and text of the post. 
 
