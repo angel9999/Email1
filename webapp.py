b6159f13c95f1a5229e23bac36d0bd2e0e91df11
Alexander Alyakrinsky <32340760+AlexanderGHA@users.noreply.github.com>
2019-02-27 12:18:43 -0800

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -54,7 +54,8 @@ def post():
     
     messages = ""
     for i in data:
-        messages += "<p>" + i['msg'] + ": " + i['usr'] + "</p>"
+        messages += "<p>" + i['usr'] + ": " + i['msg'] + "</p>"
+        
     return render_template('home.html', past_posts=Markup(messages))
     #This function should add the new post to the JSON file of posts and then render home.html and display the posts.  
     #Every post should include the username of the poster and text of the post. 
