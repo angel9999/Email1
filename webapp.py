e6e42f8ccb9c7f3ff6ca50913312f089f90f50b8
Your Name <you@example.com>
2019-03-01 10:53:02 -0800

    style

--- webapp.py
+++ webapp.py
@@ -33,7 +33,7 @@ github = oauth.remote_app(
 def posts_to_html(posts):
     messages = ""
     for i in posts:
-        messages += "<div class='posted'><p class='name'> [usr] <\p>: <p class='message'> [msg] </p></div>".replace(["[usr]", "[msg]"], [i['usr'], ['msg'].replace("\r\n", "<br>")])
+        messages += format("<div class='posted'><p class='name'> %s <\p>: <p class='message'> %s </p></div>", i['usr'], ['msg'])
     return Markup(messages)
 
 @app.context_processor
