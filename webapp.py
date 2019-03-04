7765953edbc42da207b6ce7198d3eb74b8c5df9c
Your Name <you@example.com>
2019-03-04 10:20:20 -0800

    missed the i

--- webapp.py
+++ webapp.py
@@ -33,7 +33,7 @@ github = oauth.remote_app(
 def posts_to_html(posts):
     messages = ""
     for i in posts:
-        messages += "<div class='posted'><p class='name'> %s <\p>: <p class='message'> %s </p></div>" % (i['usr'], i['msg'])
+        messages += "<div class='posted'><p class='name'> %s : <p class='message'> %s </p></div>" % (i['usr'], i['msg'])
     return Markup(messages)
 
 @app.context_processor
